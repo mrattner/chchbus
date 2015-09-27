5f10a00c1eec074eacdebf4ba0e9a9e04aaad1fc
Marcy Rattner <mar158@uclive.ac.nz>
2015-09-28 00:10:33 +1300

    Static "About" page

---
+++ ChchBus/Views/AboutPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace ChchBus {
+	/// <summary>
+	/// A static page showing info about the app.
+	/// </summary>
+	public sealed partial class AboutPage : Page {
+		public AboutPage () {
+			this.InitializeComponent();
+		}
+	}
+}
